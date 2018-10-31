41bd2897c46eb1e915edd0063834969b54ec0755
makanazilla <36710772+makanazilla@users.noreply.github.com>
2018-10-31 12:27:30 -0700

    Create AvailableSpots.py

---
+++ AvailableSpots.py
@@ -0,0 +1,14 @@
+# def get_spots (request)
+#    retnval = []
+#    if request.get
+#      return retnval
+    
+#    userloc = requst.get.copy()
+    
+#    for spot in sample.spots:
+    
+#    dist = get.dist (spot['lat'], spot['lng'], userloc['lat'], userloc['lng])
+#    if dist <= userloc['dist'] and spot['reserved']==false:
+#        retnval.push_back(spot)
+#    retun retnval
+  
